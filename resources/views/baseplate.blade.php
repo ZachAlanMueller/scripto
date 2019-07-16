1cf30d64d4fa96b0c27202da7713a41b1af29472
Zach Mueller <zach.a.mueller@gmail.com>
2019-07-15 21:39:34 -0500

    Image Max Width QF

--- resources/views/baseplate.blade.php
+++ resources/views/baseplate.blade.php
@@ -12,7 +12,11 @@
 
   <!-- Bootstrap core CSS -->
   <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
+  <style>
+    img{
+      max-width: 100%;
+    }
+  </style>
   <!-- Custom fonts for this template -->
   <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href='/css/font1.css' rel='stylesheet' type='text/css'>
