136bcec5ea088edf7295a2a1c32d13b00d3eb0ad
Zach Mueller <zach.a.mueller@gmail.com>
2019-07-17 11:03:20 -0500

    Fix the View Page Contentedittable

--- resources/views/viewPost.blade.php
+++ resources/views/viewPost.blade.php
@@ -95,7 +95,7 @@
       <div class="row">
          <div class="col-lg-8 col-md-10 mx-auto">
             <div class="site-heading">
-               <h1 id="post-title" contenteditable="true">{{$postInfo->title}}</h1>
+               <h1 id="post-title" contenteditable="false">{{$postInfo->title}}</h1>
                <span class="subheading"></span><br><br>
             </div>
          </div>
